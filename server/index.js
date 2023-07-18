19c3d7f96e772c1517ce409acd6dc464a884ae57
sakshiagar <sakshiagarwal9167@gmail.com>
2023-07-18 11:12:20 +0530

    backend setup done

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './mongodb/connect.js';//.js is just because it works in react but not in node
+
+
+import postRoutes from './routes/postRoutes.js'
+import ImageGenRoutes from "./routes/ImageGenRoutes.js";
+dotenv.config();
+
+
+const app=express();
+app.use(cors());
+app.use(express.json({limit:'50mb'}));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/imageGen", ImageGenRoutes);
+
+app.get('/', async(req,res) =>{
+    res.send('Hello from IMAGE AI');
+})
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+          app.listen(8080, () =>
+            console.log(`Server has started on port 
+            http://localhost:8080`)
+          );
+    } catch (error) {
+        console.log(error);
+    }
+  
+}
+startServer();
