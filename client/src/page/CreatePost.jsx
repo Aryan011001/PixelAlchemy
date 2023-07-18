20662c6652a5d839095a15afab9f26caa42da9a6
Aryan Ranjan <aryanranjan@Aryans-MacBook-Air.local>
2023-07-18 13:53:26 +0530

    connect backend to frontend

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -26,12 +26,61 @@ const CreatePost = () => {
 
   // backend complete karne ke baad ye hi kar payenge
   const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/imageGen', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
+        console.log(response); // Check the raw response here
 
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
   };
+
+  
+
 
   // backend complete karne ke baad ye hi kar payenge
   const handleSubmit = () => {
+    e.preventDefault();
 
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
   };
 
   return (
